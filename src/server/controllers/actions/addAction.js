cedc0176c2e7f6cc7d918f8affc0435d898bf44a
abidex4yemi <abidex4yemi@gmail.com>
2019-07-19 12:32:40 +0100

    Create add action controller function

---
+++ src/server/controllers/actions/addAction.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line import/named
+import { Action, Project } from '../../model';
+import { createError, NOT_FOUND } from '../../util/error';
+import { createSuccess, CREATED } from '../../util/success';
+
+/**
+ * Insert new action
+ *
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
+const addAction = async (req, res, next) => {
+  try {
+    // Get action from req body
+    const actionDetails = req.body.sanitizedBody;
+
+    // Check if project id is valid
+    await Project.getById(actionDetails.project_id);
+
+    // Insert new action
+    const { id } = await Action.insert(actionDetails);
+
+    // Get newly persisted action
+    const createdAction = await Action.getById(id);
+
+    return res.status(CREATED).json(
+      createSuccess({
+        data: createdAction,
+      }),
+    );
+  } catch (error) {
+    return next(
+      createError({
+        message: 'Project ID is invalid.',
+        status: NOT_FOUND,
+      }),
+    );
+  }
+};
+
+export default addAction;
--- src/server/controllers/actions/index.js
+++ src/server/controllers/actions/index.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export { default as getActions } from './getActions';
+export { default as addAction } from './addAction';
