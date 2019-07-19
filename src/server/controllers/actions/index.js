9e4fef8ac29db4fa2e545bcc2e5a729d2f1f056c
abidex4yemi <abidex4yemi@gmail.com>
2019-07-19 13:08:42 +0100

    Create delete action controller

--- src/server/controllers/actions/index.js
+++ src/server/controllers/actions/index.js
@@ -3,3 +3,4 @@ export { default as getActions } from './getActions';
 export { default as addAction } from './addAction';
 export { default as updateAction } from './updateAction';
 export { default as getActionById } from './getActionById';
+export { default as deleteAction } from './deleteAction';
