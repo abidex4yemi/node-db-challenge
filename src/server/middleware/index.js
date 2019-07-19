4e931b05c901f423c9e9013007ea800145f979ae
abidex4yemi <abidex4yemi@gmail.com>
2019-07-19 11:44:42 +0100

    Create project parameter and project body middleware

--- src/server/middleware/index.js
+++ src/server/middleware/index.js
@@ -2,3 +2,5 @@
 export { default as allErrorHandler } from './errorHandler';
 export { default as validateActionParam } from './validateActionParam';
 export { default as validateActionBody } from './validateActionBody';
+export { default as validateProjectParam } from './validateProjectParam';
+export { default as validateProjectBody } from './validateProjectBody';
